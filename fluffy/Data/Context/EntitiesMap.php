9a7325723d31161f028c63f01b2659c087cf60d8
Ivan Voitovych <col606v@gmail.com>
2025-01-04 13:38:42 +0200

    EntitiesMap

--- fluffy/Data/Context/EntitiesMap.php
+++ fluffy/Data/Context/EntitiesMap.php
@@ -4,9 +4,14 @@ namespace Fluffy\Data\Context;
 
 use Fluffy\Data\Entities\Auth\UserEntity;
 use Fluffy\Data\Entities\Auth\UserEntityMap;
+use Fluffy\Data\Entities\BaseEntityMap;
 
 class EntitiesMap
 {
+    /**
+     * 
+     * @var array<BaseEntity|string,BaseEntityMap|string>
+     */
     public static array $map = [
         UserEntity::class => UserEntityMap::class
     ];
